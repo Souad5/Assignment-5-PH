7e1d8b0562da6b623b9d96771b5553a919365e1d
Souad5 <souadalkabir@gmail.com>
2025-03-01 19:58:49 +0600

    Final

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 function getRandomColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-    for (let i = 0; i < 6; i++) {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
   }
-  
-  function changeBgColor() {
-    document.body.style.backgroundColor = getRandomColor(); 
-  }
-  
-  document.getElementById("bgColor").addEventListener("click", changeBgColor);
-  
-  
+  return color;
+}
+
+function changeBgColor() {
+  let randomColor = getRandomColor();
+  document.body.style.backgroundColor = randomColor;
+  document.getElementById("bgcolor3").style.backgroundColor = randomColor;
+
+  document.querySelector(".parent").style.backgroundColor = randomColor;
+}
+
+document.getElementById("bgColor2").addEventListener("click", changeBgColor);
